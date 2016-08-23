f94703c317d079b2311c90c6e34c53eefd1246c4
Adam Dittebrand <Adam Dittebrand>
2016-08-23 13:27:05 -0400

    1 to 100 second try

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -7,15 +7,28 @@
 # 	puts n
 # end
 
-number = 0
+# number = 0
+# 100.times do
+# 	number = number + 1
+# 	if (number % 3 == 0 and number % 5 == 0)
+# 		puts "minedminds"
+# 	elsif (number % 3 == 0)
+# 		puts "mined"
+# 	elsif (number % 5 == 0)
+# 		puts "minds"
+# 	else 
+# 		puts number
+# 	end
+# end
+
+number = 1
 100.times do
-	number = number + 1
-	if (number % 3 == 0 and number % 5 == 0)
-		puts "minedminds"
-	elsif (number % 3 == 0)
-		puts "mined"
-	elsif (number % 5 == 0)
-		puts "minds"
-	else puts number
+	if number % 3 == 0
+		puts "Mined"
+	elsif number % 5 == 0
+		puts "Minds"
+	else
+		puts number
 	end
+	number = number + 1
 end
