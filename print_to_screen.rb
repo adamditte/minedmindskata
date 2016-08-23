5aca20ec0516bd61f0ba3559ee4a20c7f18db32b
Adam Dittebrand <Adam Dittebrand>
2016-08-23 13:42:57 -0400

    1 to 100 second try final cut

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -21,9 +21,14 @@
 # 	end
 # end
 
+
+# && = and
+# || = or
 number = 1
 100.times do
-	if number % 3 == 0
+	if number % 3 == 0 && number % 5 == 0
+		puts "MinedMinds"
+	elsif number % 3 == 0
 		puts "Mined"
 	elsif number % 5 == 0
 		puts "Minds"
