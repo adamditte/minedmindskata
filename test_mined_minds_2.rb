6e4eae0bc3d08ad428b7b78ad2167fdf2b96eaea
Adam Dittebrand <Adam Dittebrand>
2016-08-26 16:12:36 -0400

    first commit (errors)

---
+++ test_mined_minds_2.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require_relative "mined_minds_2.rb"
+
+class TestMinedMindsFunction < Minitest::Test
+	def test_1_returns_1
+		assert_equal(1, mined_minds(1))
+	end
+end
+end
